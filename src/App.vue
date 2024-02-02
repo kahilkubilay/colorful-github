5ff54fa7c22524bb41c3fa980c14aac498b3567e
kahilkubilay <kahilkubilay@gmail.com>
2024-02-03 01:18:10 +0300

    all files moved

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <MainHeader />
+  <MainContent />
+  <MainFooter />
+
+</template>
+
+<script>
+import MainHeader from './components/Header/MainHeader.vue'
+import MainContent from './components/Content/MainContent.vue'
+import MainFooter from './components/Footer/MainFooter.vue'
+
+
+export default {
+  name: 'App',
+  components: { MainHeader, MainContent, MainFooter }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/Header/MainHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="../asset/colorful-github-logo.png" alt="colorful-github logo">
+    </div>
+    <div class="propaganda">
+      <h2>colorful - github</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas recusandae inventore aliquam repellendus minima placeat quis, voluptas eaque officiis ipsa veritatis laborum magnam, quia ab asperiores impedit cum quam nesciunt!</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader'
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em 0 .5em 0;
+  background-color: #f2f2f2;
+}
+.logo {
+  display: inherit;
+}
+.logo img {
+  width: 5em;
+}
+.propaganda {
+  display: inherit;
+  flex-direction: inherit;
+  align-items: inherit;
+  margin-top: 1em;
+}
+
+.propaganda h2, .propaganda p {
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+}
+
+.propaganda h2 {
+  font-size: 1.5em;
+}
+
+.propaganda p {
+  width: 50%;
+  text-align: center;
+  font-size: 1.2em;
+}
+</style>
