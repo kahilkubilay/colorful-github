0f58406e780ec24534c513f8aeadf6078a75a27b
kahilkubilay <kahilkubilay@gmail.com>
2024-02-11 00:49:45 +0300

    added npm link to footer

--- src/components/Footer/MainFooter.vue
+++ src/components/Footer/MainFooter.vue
@@ -1,7 +1,11 @@
 <template>
   <footer>
     <a href="https://github.com/kahilkubilay/colorful-github">
-      <img src="../asset/github-logo.png" alt="link of the colorful github repo"/>
+      <img src="./asset/github-logo.png" alt="link of the colorful github repo"/>
+    </a>
+
+    <a href="https://www.npmjs.com/package/colorful-github">
+      <img src="./asset/npm-logo.png" alt="link of the colorful github repo"/>
     </a>
   </footer>
 </template>
@@ -21,10 +25,16 @@ footer {
   width: 100%;
   padding: 1em;
   text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 footer a img {
   width: 2em;
+  margin-right: 1em;
+  display: inherit;
+  vertical-align: center;
 }
 
 </style>
